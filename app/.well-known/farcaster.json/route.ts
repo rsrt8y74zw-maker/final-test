4a0fac3c0f4aca87135f7ae06c9ab230926e4056
rsrt8y74zw-maker <rsrt8y74zw@privaterelay.appleid.com>
2025-10-30 17:53:32 +0100

    Create route.ts

---
+++ app/.well-known/farcaster.json/route.ts
@@ -0,0 +1,10 @@
+export async function GET() {
+  return Response.json({
+    accountAssociation: {
+      "header": "ton_header",
+      "payload": "ton_payload",
+      "signature": "ta_signature"
+    },
+  });
+}
+Create route.ts at correct path
